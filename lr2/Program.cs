fd2d2d9ac797843f4e6cfb6f2550fcfa91611504
Nikita Landarenko <landarenko_nikita@mail.ru>
2022-09-20 21:42:30 +0500

    2 lab

---
+++ lr2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace lr2
+{
+    public class Program
+    {
+        static int count = 0;
+        
+        static void Main(string[] args)
+        {
+            Str str1 = stringCreating();
+            Str str2 = stringCreating();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine("Строки созданы! Переходим в главное меню...");
+            Console.WriteLine("Примечание. Если Вы изменить строку, перезапустите программу.");
+            Console.WriteLine("При работе с программой цвет символов будет меняться для удобства чтения.");
+            Console.WriteLine();
+            Start();
+
+            bool isLaunched = true;
+
+            while (isLaunched)
+            {
+                string action = Console.ReadLine();
+
+                switch (action)
+                {
+                    // Сравнение строк
+                    case "1":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine();
+                        Console.WriteLine($"Строки равны: {str1 == str2}");
+                        Console.WriteLine($"Строки не равны: {str1 != str2}");
+                        Console.WriteLine();
+                        break;
+
+                    // Конкатенация строк
+                    case "2":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine();
+                        Console.WriteLine(str1 + str2);
+                        Console.WriteLine();
+                        break;
+
+                    // Умножение строки на n
+                    case "3":
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Выберите строку, которую Вы хотите умножить (1 или 2):");
+                        string numOfString = Console.ReadLine();
+
+                        while (numOfString != "1" && numOfString != "2")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Неверно введенные данные. Попробуйте еще раз.");
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            numOfString = Console.ReadLine();
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Введите натуральное число, на которое Вы хотите умножить строку:");
+                        bool isParsed = Int32.TryParse(Console.ReadLine(), out int n);
+
+                        while (!isParsed || n <= 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Неверно введенные данные. Попробуйте еще раз.");
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            isParsed = Int32.TryParse(Console.ReadLine(), out n);
+                            Console.WriteLine();
+                        }
+
+                        switch (numOfString)
+                        {
+                            case "1":
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine();
+                                Console.WriteLine($"Вы выбрали строку {numOfString}: " + str1);
+                                Console.WriteLine();
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(str1 * n);
+                                Console.WriteLine();
+                                break;
+
+                            case "2":
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine();
+                                Console.WriteLine($"Вы выбрали строку {numOfString}: " + str2);
+                                Console.WriteLine();
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(str2 * n);
+                                Console.WriteLine();
+                                break;
+                        }
+
+                        break;
+
+                    // Выход из программы
+                    case "exit":
+                        isLaunched = false;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine();
+                        Console.WriteLine("Такого действия нет. Введите цифру еще раз.");
+                        Console.WriteLine();
+                        break;
+                }
+
+                if (isLaunched)
+                {
+                    Start();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Работа программы завершена.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void Start()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Это начальное меню программы. Программа обладает следующими возможностями:");
+            Console.WriteLine("1. Сравнение строк;");
+            Console.WriteLine("2. Конкатенация (слияние) строк;");
+            Console.WriteLine("3. Умножение строки на число n (повторение строки n раз).\n");
+            Console.WriteLine("Действия со строками, описанные выше, пронумерованы.");
+            Console.WriteLine("Чтобы выбрать какое-либо действие, напишите цифру этого действия в консоль.");
+            Console.WriteLine("Чтобы завершить работу программы, напишите 'exit'.");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Ваш выбор: ");
+        }
+
+        static Str stringCreating()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Привет! Для начала работы нужно создать 2 строки.");
+                Console.WriteLine("Создание 1-й строки...");
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Создание 2-й строки...");
+            }
+
+            Console.WriteLine("Введите строку:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Строка создана!\n");
+
+            return new Str(str1);
+        }
+    }
+}
---
+++ lr2/Str.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace lr2
+{
+    internal class Str
+    {
+        private char[] _chars;
+
+        public Str(string chars)
+        {
+            _chars = chars.ToCharArray();
+        }
+
+        public static bool operator ==(Str chars1, Str chars2)
+        {
+            bool result = Comparison(chars1, chars2);
+            return result;
+        }
+
+        public static bool operator !=(Str chars1, Str chars2)
+        {
+            bool result = Comparison(chars1, chars2);
+            return !result;
+        }
+
+        public static string operator +(Str chars1, Str chars2)
+        {
+            char[] newStr = new char[chars1._chars.Length + chars2._chars.Length];
+
+            // Запись символов из chars1
+            for (int i = 0; i < chars1._chars.Length; i++)
+            {
+                newStr[i] = chars1._chars[i];
+            }
+
+            // Запись символов из chars2
+            int temp = chars1._chars.Length;
+            for (int i = 0; i < chars2._chars.Length; i++)
+            {
+                newStr[temp] = chars2._chars[i];
+                temp++;
+            }
+
+            return new string(newStr);
+        }
+
+        public static string operator *(Str chars, int n)
+        {
+            char[] newStr = new char[chars._chars.Length * n];
+            
+            // Добавление символов массива chars._chars в массив newStr n раз
+            int temp = 0;
+            for (int i = 0; i < newStr.Length; i++)
+            {
+                if (temp == chars._chars.Length)
+                {
+                    temp = 0;
+                }
+                
+                newStr[i] = chars._chars[temp];
+
+                temp++;
+            }
+
+            return new string(newStr);
+        }
+
+        private static bool Comparison(Str chars1, Str chars2)
+        {
+            if (chars1._chars.Length != chars2._chars.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < chars1._chars.Length; i++)
+                {
+                    if (chars1._chars[i] != chars2._chars[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return new string(_chars);
+        }
+    }
+}
